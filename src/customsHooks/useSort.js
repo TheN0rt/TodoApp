f2b38859cb325c3e61f9a0728fc2f0c558748992
TheN0rt <89389482+TheN0rt@users.noreply.github.com>
2023-04-17 14:06:51 +0300

    Update useSort.js

--- src/customsHooks/useSort.js
+++ src/customsHooks/useSort.js
@@ -3,14 +3,14 @@ import { useMemo } from "react"
 export const useSort = (todos, sortMethod) => {
     const sortedTodos = useMemo(() => {
         return sortMethod ? [...todos].sort((a,b) => a[sortMethod].localeCompare(b[sortMethod])) : todos
-      }, [todos, sortMethod]) // сделать кастомный хуки
+      }, [todos, sortMethod]) 
     return sortedTodos
 }
 
 export const useTodos = (todos, sortMethod, search) => {
     const sortedTodos = useSort(todos, sortMethod)
     const sortedAndFilteredTodos = useMemo(() => {
-        return sortedTodos.filter(e => e.title.toLowerCase().includes(search.trim().toLocaleLowerCase())) // реализовать функцию для выреза лишних пробелов между словами
+        return sortedTodos.filter(e => e.title.toLowerCase().includes(search.trim().toLocaleLowerCase())) 
       }, [search, sortedTodos])
     return sortedAndFilteredTodos
 }
